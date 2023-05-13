7d3e2d41a80a5cceae490a4db7a3c919b341237f
Mehdi Boustani <122350070+zenixtuu@users.noreply.github.com>
2023-05-13 16:04:32 +0200

    Fichiers tests + update itineraireFlamme.h

---
+++ itineraireflamme.h
@@ -0,0 +1,20 @@
+/**
+ * @file itineraireflamme.h
+ * 
+ * @brief Ce fichier contient les déclarations de types et les prototypes
+ * des fonctions pour la manipulation de l'itinéraire.
+ * 
+ * @author Boustani Mehdi/ Touhami Ibrahim
+ * @date 13 mai 2023
+ * projet INFO0947 Projet 2
+ */
+
+#ifndef __ITINERAIRE_FLAMME__
+#define __ITINERAIRE_FLAMME__
+
+
+typedef struct itineraireFlamme_t ItineraireFlamme;
+
+
+
+#endif //__ITINERAIRE_FLAMME__
